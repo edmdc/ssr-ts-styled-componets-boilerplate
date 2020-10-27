9a1b0c2fcca88ae6d731f2a19519af6e04006629
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-10-27 07:00:09 -0500

    Expand default theme types

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -2,29 +2,39 @@ import "styled-components";
 
 // extend og module declarations
 declare module "styled-components" {
+  interface ColorPalette {
+    main: string;
+    mainAccent: string;
+    secondary: string;
+    secondaryAccent: string;
+    textDark: string;
+    textLight: string;
+    black: string;
+    blue: string;
+    gray: string;
+    green: string;
+    orange: string;
+    purple: string;
+    red: string;
+    white: string;
+  }
+
+  interface Font {
+    url: string;
+    familyName: string;
+  }
+
   export interface DefaultTheme {
     borderRadius: string;
     gridGap: string;
     fonts: {
-      body: string;
-      headers: string;
+      body: Font;
+      headers: Font;
     };
 
     colors: {
-      main: string;
-      mainAccent: string;
-      secondary: string;
-      secondaryAccent: string;
-      textDark: string;
-      textLight: string;
-      black: string;
-      blue: string;
-      gray: string;
-      green: string;
-      orange: string;
-      purple: string;
-      red: string;
-      white: string;
+      dark: ColorPalette;
+      light: ColorPalette;
     };
   }
 }
